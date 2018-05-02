791ff81858a6954f292584991f63a06997dcb8b1
chuonghuynh <minhchuong.itus@gmail.com>
2018-05-02 22:13:32 +0700

    [UPDATE] Config GraphQL

---
+++ src/api/config.js
@@ -0,0 +1,8 @@
+import gql from "graphql-tag";
+
+export default {
+    serverURL: 'http://contentkit-prod.ap-southeast-1.elasticbeanstalk.com/graphql',
+    authenKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0SWQiOiI1YWRmNzRjNzdmZjQ0ZTAwMWViODI1MzkiLCJpYXQiOjE1MjQ1OTM4NjN9.Yx-17tVN1hupJeVa1sknrUKmxawuG5rx3cr8xZc7EyY',
+    queries: {
+    }
+};
---
+++ src/api/index.js
@@ -0,0 +1,46 @@
+import config from './config';
+import axios from 'axios';
+import {ApolloClient} from 'apollo-client';
+import {HttpLink} from 'apollo-link-http';
+import {onError} from 'apollo-link-error'
+import {InMemoryCache} from 'apollo-cache-inmemory';
+const instance = axios.create({
+    serverURL: `${config.serverURL}`
+});
+
+const httpLink = new HttpLink({
+    uri: config.serverURL,
+    headers: {
+        Authorization: config.authenKey}
+});
+
+const errorHandler = onError(({networkError}) => {
+    switch (networkError.statusCode) {
+        case 404:
+            return {error: {message: 'Cannot connect to server'}};
+        default:
+            return {error: {message: 'Unknown error'}};
+    }
+});
+
+const client = new ApolloClient({
+    link: errorHandler.concat(httpLink),
+    cache: new InMemoryCache()
+});
+
+const get = (endpoints) => {
+    return instance.get(`${endpoints}`)
+        .then((response) => {
+            switch (response.status) {
+                case 403:
+                    return {error: {message: 'Invalid token'}, kickOut: true};
+                case 404:
+                    return {error: {message: 'Cannot connect to server'}};
+                default:
+                    return response;
+            }
+        })
+        .catch((err) => {
+            throw err;
+        });
+};
