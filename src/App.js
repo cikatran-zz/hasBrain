6c21f0f6bd4c6f943cb9607576aa378d859fc762
Luu Duc Hieu <hieu@mstage.io>
2018-06-11 11:41:01 +0000

    App.js edited online with Bitbucket

--- src/App.js
+++ src/App.js
@@ -26,5 +26,5 @@ class App extends Component {
     }
 }
 
-let codePushOptions = { checkFrequency: codePush.CheckFrequency.MANUAL };
+let codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME };
 export default App = codePush(codePushOptions)(App)
