f4fb5a746ff1d127386c46a19d6d1559d17955c4
CiKa <cao.tranchi@gmail.com>
2018-06-28 19:55:26 +0700

    Fix crash of bookmark related to old function of rxjs

--- src/epics/bookmarkedIdsRequestEpic.js
+++ src/epics/bookmarkedIdsRequestEpic.js
@@ -1,16 +1,17 @@
 import * as actionTypes from '../actions/actionTypes'
-import 'rxjs'
-import { Observable } from 'rxjs/Observable'
+import { from, of } from 'rxjs';
+import { ofType } from 'redux-observable';
+import { mergeMap, catchError, map} from 'rxjs/operators';
 import { getBookmarkedIds } from '../api'
 import { getBookmarkedIdsFailure, getBookmarkedIdsSuccess } from '../actions/getBookmarkedIds'
 import {strings} from "../constants/strings";
 
 const getBookmarkedIdsEpic = (action$) =>
-    action$.ofType(actionTypes.FETCHING_BOOKMARKEDIDS)
-        .mergeMap(action =>
-            Observable.from(getBookmarkedIds(1, 1000))
-                .map(response => getBookmarkedIdsSuccess(response.data))
-                .catch(error => Observable.of(getBookmarkedIdsFailure(error)))
-        );
+    action$.pipe(ofType(actionTypes.FETCHING_BOOKMARKEDIDS),
+        mergeMap(action =>
+            from(getBookmarkedIds(1, 1000)).pipe(
+                map(response => getBookmarkedIdsSuccess(response.data)),
+                catchError(error => of(getBookmarkedIdsFailure(error)))
+        )));
 
 export default getBookmarkedIdsEpic
